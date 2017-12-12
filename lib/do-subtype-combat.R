1c74c966ee2df15c09c50ccdb65a4eeea9c13dda
Gil@StKilda <gil.tms@gmail.com>
2017-12-12 12:12:28 +0000

    [lib/do-subtype-combat.R] Rewrite, simplify

--- lib/do-subtype-combat.R
+++ lib/do-subtype-combat.R
@@ -2,46 +2,58 @@
 ## columns) of at least two datasets; a factor describing each dataset; and a
 ## factor of clinical subtypes as derived by an external classifier, returns an
 ## integrated matrix of expression, the result of stacking ComBat outputs for
-## the integration of all datasets split by clinical subtypes.
+## the integration of all datasets split by molecular clinical subtypes.
 
 ## * doSubtypeCombat
 doSubtypeCombat <- function(mtx, # concatenated expression matrix of at least
                                         # two datasets
                             d.fac, # factor of length ncol(mtx) describing
-                                        # dataset batches
-                            subtypePreds.fac,# factor of subtype predictions,
-                                        # of length ncol(mtx)
+                                        # datasets, with at least two levels
+                            subtypePreds.fac, # factor of subtype predictions,
+                                        # of length ncol(mtx), with at least two
+                                        # levels
                             nMin = 3) { # minimum number of samples required per
                                         # subtype-specific batch
     sampleInfo.dtb <- data.table(sn = colnames(mtx),
                                  b = d.fac,
                                  st = subtypePreds.fac)
-    ## Some requirements have to be met for ComBat to run.  Each
-    ## subtype-specific batch must have at least 2 samples (although we require
-    ## nMin = 3 to be more stringent) to draw summary statistics from: remove
-    ## any sample that contibutes to violate this condition.  Each subtype must
-    ## encompass at least two batches, i.e., no given subtype can only
-    ## integrate samples from one batch: remove all samples that contribute to
-    ## violate this condition.
-    lst <- tapply(1:ncol(mtx),
-                  subtypePreds.fac, function(x) {
-                      list(d = mtx[, x],
-                           b = d.fac[x])
-                  })
-    nSamples.lst <- lapply(lst, function(l) table(l$b))
-    samples2rm.lst <- lapply(nSamples.lst, function(sbt) {
-        res <- sbt < nMin
-        if(sum(!res) < 2) {
-            return(setNames(rep(TRUE, length(sbt)),
-                            names(sbt)))
-        } else {
-            return(res)
-        }
-    })
+    ## Some requirements have to be met for ComBat to run:
+    ## 1. Each subtype-specific batch must have at least 2 samples (although we
+    ## require nMin = 3 to be more stringent) to draw summary statistics from:
+    ## remove any sample that contibutes to violate this condition.
+    tableSampleInfo.dtb <- sampleInfo.dtb[, .N, by = .(b, st)]
+    setkey(tableSampleInfo.dtb, b, st)
+    setkey(sampleInfo.dtb, b, st)
+    sn1 <- sampleInfo.dtb[tableSampleInfo.dtb[N >= nMin]][, sn]
+    ## 2. Each subtype must encompass at least two batches, i.e., no given
+    ## subtype can only integrate samples from one batch: remove all samples
+    ## that contribute to violate this condition.
+    tableSubtypeCrossBatch.dtb <- tableSampleInfo.dtb[, .N, by = st]
+    setkey(tableSubtypeCrossBatch.dtb, st)
+    setkey(sampleInfo.dtb, st)
+    sn2 <- sampleInfo.dtb[tableSubtypeCrossBatch.dtb[N > 1]][, sn]
+    ## intersect both sets of samples
+    validSn <- intersect(sn1, sn2)
+    ## lst <- tapply(1:ncol(mtx),
+    ##               subtypePreds.fac, function(x) {
+    ##                   list(d = mtx[, x],
+    ##                        b = d.fac[x])
+    ##               })
+    ## nSamples.lst <- lapply(lst, function(l) table(l$b))
+    ## samples2rm.lst <- lapply(nSamples.lst, function(sbt) {
+    ##     res <- sbt < nMin
+    ##     if(sum(!res) < 2) {
+    ##         return(setNames(rep(TRUE, length(sbt)),
+    ##                         names(sbt)))
+    ##     } else {
+    ##         return(res)
+    ##     }
+    ## })
     ## define index of samples that meet required conditions
-    idx2kp <- mapply(function(st, b) !samples2rm.lst[[st]][[b]],
-                     st = sampleInfo.dtb[["st"]],
-                     b = sampleInfo.dtb[["b"]])
+    idx2kp <- colnames(mtx) %in% validSn
+    ## idx2kp <- mapply(function(st, b) !samples2rm.lst[[st]][[b]],
+    ##                  st = sampleInfo.dtb[["st"]],
+    ##                  b = sampleInfo.dtb[["b"]])
     ## redefine list of subtype-specific batches only with samples that meet
     ## required conditions
     rdx.lst <- tapply(1:ncol(mtx[, idx2kp]),
